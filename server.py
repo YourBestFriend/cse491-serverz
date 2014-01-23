f6c129b9fa5c7e7791a7d846c0eff36dd85493ad
Student <student@ubuntu.(none)>
2014-01-23 02:06:53 -0800

    hw2 basically done

--- server.py
+++ server.py
@@ -3,42 +3,50 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn()     # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+	s = socket.socket()         # Create a socket object
+	host = socket.getfqdn()     # Get local machine name
+	port = random.randint(8000, 9999)
+	s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+	print 'Starting server on', host, port
+	print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+	s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print c.recv(1000)
-    print 'Got connection from', client_host, client_port
-    c.send("HTTP/1.0 200 OK\r\n")
-    c.send("Content-type: text/html\r\n\r\n")
-    c.send('''
-	<html style='background-color: black;'>
+	print 'Entering infinite loop; hit CTRL-C to exit'
+	while True:
+	    # Establish connection with client.    
+	    c, (client_host, client_port) = s.accept()
+	    handle_connection(c)
 
-		<body style='border: 5px solid brown; 
-				border-radius: 500px; 
-				background-color: tan; 
-				font-style: italic; 
-				padding: 50px; 
-				display: inline-block; 
-				position: absolute; 
-				left: 25%; 
-				top: 25%;'>
+def handle_connection(conn):
+	requestData = conn.recv(1000)
+    	getPost = requestData.split()[0]
+    	path = requestData.split()[1] 
 
-		  	<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>
+   	#print 'Got connection from', client_host, client_port  <<<<NO WORKY!!!
+    	print 'Connection Acquired.'
 
-		 	<p style='color: dimgray;'>This here is YourBestFriend's Web server!!!</p>
 
-			<script type='text/javascript'>
+	#=========================== PAGE CONTENT =================================================
+	content1 = """<html style='background-color: black;'>
+
+			<body style='border: 5px solid brown; 
+					border-radius: 500px; 
+					background-color: tan; 
+					font-style: italic; 
+					padding: 50px; 
+					display: inline-block; 
+					position: absolute; 
+					left: 25%; 
+					top: 25%;'>"""
+
+			 #<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>
+
+	content2 = """    <p style='color: dimgray;'>This here is YourBestFriend's Web server!!!</p>
+
+			  <script type='text/javascript'>
 				window.setInterval(function(){ changeColor(); }, 75);
 				var color = 'red';
 
@@ -56,9 +64,57 @@ while True:
 					else if(color == 'cyan'){color = 'indigo';} 
 					else if(color == 'indigo'){color = 'red';}   
 				}
-			</script>
+			  </script>
 
-		</body>
-	</html>
-	''')
-    c.close()
+		        </body>
+	              </html>"""
+	#==========================================================================================
+
+
+    	if getPost == 'POST':
+		conn.send("HTTP/1.0 200 OK\r\n")
+	    	conn.send("Content-type: text/html\r\n\r\n")
+	    	conn.send(content1)
+		conn.send("<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>")
+		conn.send(content2)
+		conn.close()
+
+	elif getPost == 'GET':
+		if path == '/':
+			conn.send("HTTP/1.0 200 OK\r\n")
+		    	conn.send("Content-type: text/html\r\n\r\n")
+		    	conn.send(content1)
+			conn.send("<h1 id='header' style='color: black; opacity: .5;'>ZZZzzzz -_-</h1>")
+			conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/content'>Content</a>")
+			conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/file'>File</a>")
+			conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/image'>Image</a>")
+			conn.send(content2)
+			conn.close()
+
+		elif path == '/content':
+			conn.send("HTTP/1.0 200 OK\r\n")
+		    	conn.send("Content-type: text/html\r\n\r\n")
+		    	conn.send(content1)
+			conn.send("<h1 id='header' style='color: black; opacity: .5;'>Content ^_^</h1>")
+			conn.send(content2)
+			conn.close()
+
+		elif path == '/file':
+			conn.send("HTTP/1.0 200 OK\r\n")
+		    	conn.send("Content-type: text/html\r\n\r\n")
+		    	conn.send(content1)
+			conn.send("<h1 id='header' style='color: black; opacity: .5;'>File ^_^</h1>")
+			conn.send(content2)
+			conn.close()
+
+		elif path == '/image':
+			conn.send("HTTP/1.0 200 OK\r\n")
+		    	conn.send("Content-type: text/html\r\n\r\n")
+		    	conn.send(content1)
+			conn.send("<h1 id='header' style='color: black; opacity: .5;'>Image ^_^</h1>")
+			conn.send(content2)
+			conn.close()
+			
+
+if __name__ == '__main__':
+   main()
--- test_server.py
+++ test_server.py
@@ -1,5 +1,46 @@
 import server
 
+	#=========================== PAGE CONTENT =================================================
+	content1 = """<html style='background-color: black;'>
+
+			<body style='border: 5px solid brown; 
+					border-radius: 500px; 
+					background-color: tan; 
+					font-style: italic; 
+					padding: 50px; 
+					display: inline-block; 
+					position: absolute; 
+					left: 25%; 
+					top: 25%;'>"""
+
+			 #<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>
+
+	content2 = """    <p style='color: dimgray;'>This here is YourBestFriend's Web server!!!</p>
+
+			  <script type='text/javascript'>
+				window.setInterval(function(){ changeColor(); }, 75);
+				var color = 'red';
+
+				function changeColor()
+				{ 
+					document.getElementById('header').style.color= color; newColor();
+				}
+
+				function newColor()
+				{   
+					if(color == 'red'){color = 'darkorange';} 
+					else if(color == 'darkorange'){color = 'yellow';} 
+					else if(color == 'yellow'){color = 'chartreuse';} 
+					else if(color == 'chartreuse'){color = 'cyan';} 
+					else if(color == 'cyan'){color = 'indigo';} 
+					else if(color == 'indigo'){color = 'red';}   
+				}
+			  </script>
+
+		        </body>
+	              </html>"""
+	#==========================================================================================
+
 class FakeConnection(object):
     """
     A fake connection class that mimics a real TCP socket for the purpose
@@ -25,15 +66,79 @@ class FakeConnection(object):
     def close(self):
         self.is_closed = True
 
-# Test a basic GET call.
 
-def test_handle_connection():
+
+# Test GET / call.
+def test_handle_connection_get():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    expected_return = 'HTTP/1.0 200 OK\r\n' + \
-                      'Content-type: text/html\r\n' + \
-                      '\r\n' + \
-                      '<h1>Hello, world.</h1>' + \
-                      'This is ctb\'s Web server.'
+    expected_return = "HTTP/1.0 200 OK\r\n" + \
+		      "Content-type: text/html\r\n\r\n" + \
+		      content1 + \
+	              "<h1 id='header' style='color: black; opacity: .5;'>ZZZzzzz -_-</h1>" + \
+		      content2
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test GET /content call.
+def test_handle_connection_content():
+    conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
+    expected_return = "HTTP/1.0 200 OK\r\n" + \
+		      "Content-type: text/html\r\n\r\n" + \
+		      content1 + \
+	              "<h1 id='header' style='color: black; opacity: .5;'>Content ^_^</h1>" + \
+		      content2
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test GET /file call.
+def test_handle_connection_file():
+    conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
+    expected_return = "HTTP/1.0 200 OK\r\n" + \
+		      "Content-type: text/html\r\n\r\n" + \
+		      content1 + \
+	              "<h1 id='header' style='color: black; opacity: .5;'>File ^_^</h1>" + \
+                      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/content'>Content</a>" + \
+		      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/file'>File</a>" + \
+		      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/image'>Image</a>" + \
+		      content2
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test GET /image call.
+def test_handle_connection_image():
+    conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
+    expected_return = "HTTP/1.0 200 OK\r\n" + \
+		      "Content-type: text/html\r\n\r\n" + \
+		      content1 + \
+	              "<h1 id='header' style='color: black; opacity: .5;'>Image ^_^</h1>" + \
+		      content2
+
+    server.handle_connection(conn)
+
+    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test POST / call.
+def test_handle_connection_post():
+    conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
+    expected_return = "HTTP/1.0 200 OK\r\n" + \
+		      "Content-type: text/html\r\n\r\n" + \
+		      content1 + \
+	              "<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>" + \
+		      content2
 
     server.handle_connection(conn)
 
