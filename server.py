c39f9ff666b4b2e6e5663b00c2c6cc56fc261f49
Student <student@ubuntu.(none)>
2014-02-06 00:31:47 -0800

    hw4 Commit 1

--- server.py
+++ server.py
@@ -3,6 +3,10 @@ import random
 import socket
 import time
 import urlparse
+import cgi
+import StringIO
+import jinja2
+
 
 def main():
 	s = socket.socket()         # Create a socket object
@@ -21,8 +25,24 @@ def main():
 	    c, (client_host, client_port) = s.accept()
 	    handle_connection(c)
 
+
+
 def handle_connection(conn):
-	requestData = conn.recv(1000)
+	fileSystemLoader = jinja2.FileSystemLoader('./templates')
+	environment = jinja2.Environment(loader=fileSystemLoader)
+
+	#This header gathering idea was taken from:
+	#https://github.com/cameronkeif/cse491-serverz/blob/hw4/server.py
+	#Before using this code any additional calls to conn.recv() would 
+	#cause the server to hang.
+	#----------------------------------------------------------------
+	requestData = conn.recv(1)
+	
+  	# This will get all the headers
+  	while requestData[-4:] != '\r\n\r\n':
+    		requestData += conn.recv(1)
+	#----------------------------------------------------------------
+
     	
    	#print 'Got connection from', client_host, client_port  <<<<NO WORKY!!!
     	print requestData
@@ -33,146 +53,135 @@ def handle_connection(conn):
 	parsedPath = urlparse.urlparse(firstLine[1])
 	path = parsedPath[2]
 
-	
 
-	#=========================== PAGE CONTENT =================================================
-	content1 = """<html style='background-color: black;'>
-
-			<body style='border: 5px solid brown; 
-					border-radius: 500px; 
-					background-color: tan; 
-					font-style: italic; 
-					padding: 50px; 
-					display: inline-block; 
-					position: absolute; 
-					left: 25%; 
-					top: 25%;'>"""
-
-			 #<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>
-
-	content2 = """    <p style='color: dimgray;'>This here is YourBestFriend's Web server!!!</p>
-
-			  <script type='text/javascript'>
-				window.setInterval(function(){ changeColor(); }, 75);
-				var color = 'red';
-
-				function changeColor()
-				{ 
-					document.getElementById('header').style.color= color; newColor();
-				}
-
-				function newColor()
-				{   
-					if(color == 'red'){color = 'darkorange';} 
-					else if(color == 'darkorange'){color = 'yellow';} 
-					else if(color == 'yellow'){color = 'chartreuse';} 
-					else if(color == 'chartreuse'){color = 'cyan';} 
-					else if(color == 'cyan'){color = 'indigo';} 
-					else if(color == 'indigo'){color = 'red';}   
-				}
-			  </script>
-
-		        </body>
-	              </html>"""
-	#==========================================================================================
 
 
     	if getPost == 'POST':
+		#fill the postHeaders dictionary--------
+		postHeaders = {}
+
+		requestList = requestData.split('\r\n')
+
+		i=1
+		while ":" in requestList[i]:
+			requestLine = requestList[i].split(": ")			
+			postHeaders[requestLine[0]] = requestLine[1]
+			i += 1
+		#---------------------------------------
+		
+
+		#get postContent------------------------
+		contentLength = int(postHeaders['Content-Length'])
+		postContent = conn.recv(contentLength)
+		#---------------------------------------
+
+		
+		FS = cgi.FieldStorage(headers = postHeaders, \
+				      fp = StringIO.StringIO(postContent), \
+				      environ = {'REQUEST_METHOD': 'POST'})
+
+
 		if path == '/':
-			generatePost(conn, content1, content2)
+			generatePost(conn, environment)
+
 		elif path == '/submit':
-			generatePostSubmit(conn, content1, content2, requestData.split('\r\n')[-1])
+			generatePostSubmit(conn, environment, FS)
+
+		else:
+			generateNotFound(conn, environment)
 
 	elif getPost == 'GET':
 		if path == '/':
-			generateGet(conn, content1, content2)
+			generateGet(conn, environment)
 
 		elif path == '/content':
-			generateGetContent(conn, content1, content2)
+			generateGetContent(conn, environment)
 
 		elif path == '/file':
-			generateGetFile(conn, content1, content2)
+			generateGetFile(conn, environment)
 
 		elif path == '/image':
-			generateGetImage(conn, content1, content2)
+			generateGetImage(conn, environment)
 
 		elif path == '/submit':
-			generateGetSubmit(conn, content1, content2, parsedPath[4])
+			generateGetSubmit(conn, environment, parsedPath[4])
+
+		else:
+			generateNotFound(conn, environment)
+
+
+
+
+
 		
 
-def generatePost(conn, content1, content2):
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>")
-	conn.send(content2)
+def generatePost(conn, environment):
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("post.html").render()
+	conn.send(reply)
 	conn.close()
 
-def generatePostSubmit(conn, content1, content2, parsedPath):
+def generatePostSubmit(conn, environment, FS):	
+	firstName = FS['firstname'].value
+	lastName = FS['lastname'].value
+	
+	data = {'firstName': firstName, 'lastName': lastName}
+
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("postSubmit.html").render(data)
+	conn.send(reply)
+	conn.close()
+
+def generateGet(conn, environment):
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("get.html").render()
+	conn.send(reply)
+	conn.close()
+
+def generateGetContent(conn, environment):
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("getContent.html").render()
+	conn.send(reply)
+	conn.close()
+
+def generateGetFile(conn, environment):
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("getFile.html").render()
+	conn.send(reply)
+	conn.close()
+
+def generateGetImage(conn, environment):
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("getImage.html").render()
+	conn.send(reply)
+	conn.close()
+
+def generateGetSubmit(conn, environment, parsedPath):
 	parsedPath = urlparse.parse_qs(parsedPath);
 	
 	firstName = parsedPath['firstname'][0]
 	lastName = parsedPath['lastname'][0]
 	
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>POST Submit</h1>")
-	conn.send("Howdy thar Mr. %s %s!" % (firstName, lastName))
-	conn.send(content2)
+	data = {'firstName': firstName, 'lastName': lastName}
+
+	reply = "HTTP/1.0 200 OK\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("getSubmit.html").render(data)
+	conn.send(reply)
 	conn.close()
 
-def generateGet(conn, content1, content2):
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>ZZZzzzz -_-</h1>")
-	conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/content'>Content</a>")
-	conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/file'>File</a>")
-	conn.send("<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/image'>Image</a>")
-	conn.send("<p>GET Name Submission</p><form action='/submit' method='GET'><input style='width: 150px' type='text' name='firstname'><input style='width: 150px' type='text' name='lastname'><input type='submit' value='Submit'></form>")
-	conn.send("<p>POST Name Submission</p><form action='/submit' method='POST'><input style='width: 150px'type='text' name='firstname'><input style='width: 150px' type='text' name='lastname'><input type='submit' value='Submit'></form>")
-	conn.send(content2)
+def generateNotFound(conn, environment):
+	reply = "HTTP/1.0 404 Not Found\r\n" + \
+		"Content-type: text/html\r\n\r\n" + \
+		environment.get_template("notFound.html").render()
+	conn.send(reply)
 	conn.close()
-
-def generateGetContent(conn, content1, content2):
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>Content ^_^</h1>")
-	conn.send(content2)
-	conn.close()
-
-def generateGetFile(conn, content1, content2):
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>File ^_^</h1>")
-	conn.send(content2)
-	conn.close()
-
-def generateGetImage(conn, content1, content2):
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>Image ^_^</h1>")
-	conn.send(content2)
-	conn.close()
-
-def generateGetSubmit(conn, content1, content2, parsedPath):
-	parsedPath = urlparse.parse_qs(parsedPath);
-	
-	firstName = parsedPath['firstname'][0]
-	lastName = parsedPath['lastname'][0]
-	
-	conn.send("HTTP/1.0 200 OK\r\n")
-    	conn.send("Content-type: text/html\r\n\r\n")
-    	conn.send(content1)
-	conn.send("<h1 id='header' style='color: black; opacity: .5;'>GET Submit</h1>")
-	conn.send("Howdy thar Mr. %s %s!" % (firstName, lastName))
-	conn.send(content2)
-	conn.close()
-
 	
 
 if __name__ == '__main__':
--- test_server.py
+++ test_server.py
@@ -1,45 +1,5 @@
 import server
 
-	#=========================== PAGE CONTENT =================================================
-content1 = """<html style='background-color: black;'>
-
-			<body style='border: 5px solid brown; 
-					border-radius: 500px; 
-					background-color: tan; 
-					font-style: italic; 
-					padding: 50px; 
-					display: inline-block; 
-					position: absolute; 
-					left: 25%; 
-					top: 25%;'>"""
-
-			 #<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>
-
-content2 = """    <p style='color: dimgray;'>This here is YourBestFriend's Web server!!!</p>
-
-			  <script type='text/javascript'>
-				window.setInterval(function(){ changeColor(); }, 75);
-				var color = 'red';
-
-				function changeColor()
-				{ 
-					document.getElementById('header').style.color= color; newColor();
-				}
-
-				function newColor()
-				{   
-					if(color == 'red'){color = 'darkorange';} 
-					else if(color == 'darkorange'){color = 'yellow';} 
-					else if(color == 'yellow'){color = 'chartreuse';} 
-					else if(color == 'chartreuse'){color = 'cyan';} 
-					else if(color == 'cyan'){color = 'indigo';} 
-					else if(color == 'indigo'){color = 'red';}   
-				}
-			  </script>
-
-		        </body>
-	              </html>"""
-	#==========================================================================================
 
 class FakeConnection(object):
     """
@@ -71,110 +31,100 @@ class FakeConnection(object):
 # Test GET / call.
 def test_generateGet():
     conn = FakeConnection("GET / HTTP/1.0\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>ZZZzzzz -_-</h1>" + \
-		      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/content'>Content</a>" + \
-		      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/file'>File</a>" + \
-		      "<a style='color: DarkSlateGray; margin: 15px; text-decoration: none;' href='/image'>Image</a>" + \
-		      "<p>GET Name Submission</p><form action='/submit' method='GET'><input style='width: 150px' type='text' name='firstname'><input style='width: 150px' type='text' name='lastname'><input type='submit' value='Submit'></form>" + \
-		      "<p>POST Name Submission</p><form action='/submit' method='POST'><input style='width: 150px'type='text' name='firstname'><input style='width: 150px' type='text' name='lastname'><input type='submit' value='Submit'></form>" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'GET Name Submission' in conn.sent and \
+	   'POST Name Submission' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test GET /content call.
 def test_generateGetContent():
     conn = FakeConnection("GET /content HTTP/1.0\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>Content ^_^</h1>" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'Content ^_^' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test GET /file call.
 def test_generateGetFile():
     conn = FakeConnection("GET /file HTTP/1.0\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>File ^_^</h1>" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'File ^_^' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test GET /image call.
 def test_generateGetImage():
     conn = FakeConnection("GET /image HTTP/1.0\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>Image ^_^</h1>" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'Image ^_^' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test GET /submit call.
 def test_generateGetSubmit():
     conn = FakeConnection("GET /submit?firstname=Bill&lastname=Nye HTTP/1.0HTTP/1.1\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>GET Submit</h1>" + \
-		      "Howdy thar Mr. Bill Nye!" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'GET Submit' in conn.sent and \
+	   'Howdy thar Mr. Bill Nye!' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test POST / call.
 def test_generatePost():
     conn = FakeConnection("POST / HTTP/1.0\r\n\r\n")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>Howdy World ^_^</h1>" + \
-		      content2
-
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'Howdy World ^_^' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
 
 
 
 # Test POST /submit call.
 def test_generatePostSubmit():
-    conn = FakeConnection("POST /submit HTTP/1.1\r\n\r\nfirstname=Bill&lastname=Nye")
-    expected_return = "HTTP/1.0 200 OK\r\n" + \
-		      "Content-type: text/html\r\n\r\n" + \
-		      content1 + \
-	              "<h1 id='header' style='color: black; opacity: .5;'>POST Submit</h1>" + \
-		      "Howdy thar Mr. Bill Nye!" + \
-		      content2
-
+    conn = FakeConnection("POST /submit HTTP/1.1\r\n\r\nfirstname=Heyro&lastname=Duncan")
     server.handle_connection(conn)
 
-    assert conn.sent == expected_return, 'Got: %s' % (repr(conn.sent),)
+    assert 'HTTP/1.0 200' in conn.sent and \
+	   'POST Submit' in conn.sent and \
+	   'Howdy thar Mr. Heyro Duncan!' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test Post Not Found.
+def test_PostgenerateNotFound():
+    conn = FakeConnection("POST /applesauce HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+
+    assert 'HTTP/1.0 404 Not Found' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
+
+
+
+# Test Get Not Found.
+def test_GetgenerateNotFound():
+    conn = FakeConnection("GET /applesauce HTTP/1.0\r\n\r\n")
+    server.handle_connection(conn)
+
+    assert 'HTTP/1.0 404 Not Found' in conn.sent, \
+    'Got: %s' % (repr(conn.sent),)
+
+
 
